2f7cebae8c03aa6821d1260634423ae59ddeef8f
issTyTy <115404049+issTyTy@users.noreply.github.com>
2023-03-12 21:00:03 +0200

    functional updated
    fixed bugs

--- app.js
+++ app.js
@@ -2,132 +2,131 @@
 
 const express = require("express");
 const app = express();
-const bcrypt = require("bcrypt")
-const passport = require('passport');
-const initializePassport = require("./passport-config")
-const port = 5000;
-app.set('view engine', 'ejs')
-app.use(express.static('public'))
+const bcrypt = require("bcrypt");
+const passport = require("passport");
+const initializePassport = require("./passport-config");
+const port = 4000;
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(express.json());
 
+const users = [];
 
-const users = []
+app.use(express.urlencoded({ extended: false }));
 
-app.use(express.urlencoded({extended: false}))
+app.get("/test", (req, res) => {
+  res.json("ok");
+});
 
-app.post("register", async (req, res) => {
+app.post("/register", async (req, res) => {
+  const { name, email, password } = req.body;
   try {
-    const hashedPassword = await bcrypt.hash(req.body.password, 10)
+    console.log(req.body.password);
+    const hashedPassword = await bcrypt.hash(password, 10);
     users.push({
       id: Date.now().toString(),
-      name: req.body.name,
-      email: req.body.email,
-      password: hashedPassword
-    })
-    res.redirect("/login")
-    
+      name,
+      email,
+      password: hashedPassword,
+    });
+    // res.redirect("/login")
+    console.log(users);
   } catch (e) {
-    
-    res.redirect('/register')
+    res.redirect("/register");
   }
-  console.log(e);
-})
-
+  res.json("ok");
+});
 
 //for auto refresh
 const path = require("path");
 const livereload = require("livereload");
 const liveReloadServer = livereload.createServer();
-liveReloadServer.watch(path.join(__dirname, 'public'));
- 
- 
+liveReloadServer.watch(path.join(__dirname, "public"));
+
 const connectLivereload = require("connect-livereload");
 app.use(connectLivereload());
- 
+
 liveReloadServer.server.once("connection", () => {
   setTimeout(() => {
     liveReloadServer.refresh("/");
   }, 100);
-}); 
-
+});
 
 // mongoose connection
-const mongoose = require('mongoose');
-mongoose.set('strictQuery', false);
- mongoose.connect("mongodb+srv://TyTy:mody2000@cluster0.hwyb70a.mongodb.net/all-data?retryWrites=true&w=majority")
-  .then( result => {
+const mongoose = require("mongoose");
+mongoose.set("strictQuery", false);
+mongoose
+  .connect(
+    "mongodb+srv://TyTy:mody2000@cluster0.hwyb70a.mongodb.net/all-data?retryWrites=true&w=majority"
+  )
+  .then((result) => {
     app.listen(3000);
   })
-  .catch( err => {
+  .catch((err) => {
     console.log(err);
-  }); 
+  });
 
-
-
-//HTTP routing
-
+// HTTP routing
 
 app.get("/login", (req, res) => {
-  res.render("login")
+  res.render("login");
 });
 
 app.get("/register", (req, res) => {
-  res.render("register.ejs")
+  res.render("register.ejs");
 });
 
-
-
 app.get("/", (req, res) => {
-  res.render("index")
+  res.render("index");
 });
 
 app.get("/products/cpu/", (req, res) => {
-  res.render("Cpu")
+  res.render("Cpu");
 });
 
 app.get("/products/cpu-cooler/", (req, res) => {
-  res.render("Cpu Cooler")
+  res.render("Cpu Cooler");
 });
 
 app.get("/products/motherboard/", (req, res) => {
-  res.render("Motherboards")
+  res.render("Motherboards");
 });
 
 app.get("/products/memory/", (req, res) => {
-  res.render("Memory")
+  res.render("Memory");
 });
 
 app.get("/products/internal-hard-drive/", (req, res) => {
-  res.render("Storage")
+  res.render("Storage");
 });
 
 app.get("/products/video-card/", (req, res) => {
-  res.render("Video Card")
+  res.render("Video Card");
 });
 
 app.get("/products/case/", (req, res) => {
-  res.render("Cases")
+  res.render("Cases");
 });
 
 app.get("/products/power-supply/", (req, res) => {
-  res.render("Power Supply")
+  res.render("Power Supply");
 });
 
 app.get("/products/os/", (req, res) => {
-  res.render("Operating System")
+  res.render("Operating System");
 });
 
 app.get("/products/monitor/", (req, res) => {
-  res.render("Monitor")
+  res.render("Monitor");
 });
 
 app.get("/about", (req, res) => {
-  res.render("about")
+  res.render("about");
 });
 
 app.get("/contact", (req, res) => {
-  res.render("contact")
+  res.render("contact");
 });
-
 
 //  404 Erorr
 app.use((req, res) => {
@@ -135,8 +134,6 @@ app.use((req, res) => {
 });
 
 // End Routes
-
-console.log(users);
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
