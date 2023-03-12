93dddd05308e6dcb4ad717876815ac1713ef471b
issTyTy <115404049+issTyTy@users.noreply.github.com>
2023-03-12 12:24:38 +0200

    added sites
    added <login/signUp-by yami> , <about us/contact-by error>

--- app.js
+++ app.js
@@ -1,10 +1,37 @@
-//  to controll ur website
+// Importing Libraies installed using npm
 
 const express = require("express");
 const app = express();
+const bcrypt = require("bcrypt")
+const passport = require('passport');
+const initializePassport = require("./passport-config")
 const port = 5000;
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
+
+
+const users = []
+
+app.use(express.urlencoded({extended: false}))
+
+app.post("register", async (req, res) => {
+  try {
+    const hashedPassword = await bcrypt.hash(req.body.password, 10)
+    users.push({
+      id: Date.now().toString(),
+      name: req.body.name,
+      email: req.body.email,
+      password: hashedPassword
+    })
+    res.redirect("/login")
+    
+  } catch (e) {
+    
+    res.redirect('/register')
+  }
+  console.log(e);
+})
+
 
 //for auto refresh
 const path = require("path");
@@ -37,6 +64,16 @@ mongoose.set('strictQuery', false);
 
 
 //HTTP routing
+
+
+app.get("/login", (req, res) => {
+  res.render("login")
+});
+
+app.get("/register", (req, res) => {
+  res.render("register.ejs")
+});
+
 
 
 app.get("/", (req, res) => {
@@ -83,12 +120,23 @@ app.get("/products/monitor/", (req, res) => {
   res.render("Monitor")
 });
 
+app.get("/about", (req, res) => {
+  res.render("about")
+});
+
+app.get("/contact", (req, res) => {
+  res.render("contact")
+});
+
+
 //  404 Erorr
 app.use((req, res) => {
   res.status(404).send("Whoops cant find that!");
 });
 
+// End Routes
 
+console.log(users);
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
