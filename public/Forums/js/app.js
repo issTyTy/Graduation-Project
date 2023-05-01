089d22d2e0e55395e2a1262ba78fdc9e9cd9dd9e
AhmedNader2001 <ahmed25jan211@gmail.com>
2023-05-01 16:18:56 +0300

    the cum update
    now you can chat in discord with the website

--- public/Forums/js/app.js
+++ public/Forums/js/app.js
@@ -108,3 +108,10 @@ function fetchComments () {
 }
 
 window.onload = fetchComments
+
+<script src="https://cdn.jsdelivr.net/npm/@widgetbot/crate@3" async defer>
+  new Crate({
+    server: '299881420891881473',
+    channel: '355719584830980096',
+  })
+</script>
